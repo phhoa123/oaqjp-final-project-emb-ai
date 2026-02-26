325254c697196fde13204d212b22a55e00928397
phhoa79 <phhoa79@gmail.com>
2026-02-25 22:02:20 -0500

    Add EmotionDetection package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = {"raw_document": {"text": text_to_analyse}}
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    data = response.json()
+
+    emotions = data["emotionPredictions"][0]["emotion"]
+
+    result = {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+    }
+
+    result["dominant_emotion"] = max(result, key=result.get)
+    return result
